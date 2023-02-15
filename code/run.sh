f630d150c8458284711aba1c99cca5ab28051deb
Sarthak Mittal <sarthakmittal0902@gmail.com>
2023-02-15 23:37:23 +0530

    N-experiments

--- code/run.sh
+++ code/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "./sim -n 10 --slow 0.3 --low 0.3 --txn_interarrival_mean 100 --blk_interarrival_mean 600 --min_ngbrs 4 --max_ngbrs 8 --seed 0 --sim_time 10000 > ../outfile" >> ../commands.txt
-./sim -n 10 --slow 0.3 --low 0.3 --txn_interarrival_mean 100 --blk_interarrival_mean 600 --min_ngbrs 4 --max_ngbrs 8 --seed 0 --sim_time 10000 > ../outfile
+echo "./sim -n 20 --slow 0.3 --low 0.3 --txn_interarrival_mean 100 --blk_interarrival_mean 600 --min_ngbrs 4 --max_ngbrs 8 --seed 0 --sim_time 10000 > ../outfile" >> ../commands.txt
+./sim -n 20 --slow 0.3 --low 0.3 --txn_interarrival_mean 100 --blk_interarrival_mean 600 --min_ngbrs 4 --max_ngbrs 8 --seed 0 --sim_time 10000 > ../outfile
 ./files.sh $1
