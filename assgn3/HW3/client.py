ad158128e5c04739167d2b450541c400558e25ae
Sarthak Mittal <sarthakmittal0902@gmail.com>
2023-04-06 10:50:10 +0530

    Adding assignment 3

---
+++ assgn3/HW3/client.py
@@ -0,0 +1,36 @@
+import json
+from web3 import Web3
+
+
+#connect to the local ethereum blockchain
+provider = Web3.HTTPProvider('http://127.0.0.1:8545')
+w3 = Web3(provider)
+#check if ethereum is connected
+print(w3.is_connected())
+
+#replace the address with your contract address (!very important)
+deployed_contract_address = '0x8F046F919BA03C951C9E6c1A5439f6C62F7341bd'
+
+#path of the contract json file. edit it with your contract json file
+compiled_contract_path ="build/contracts/Payment.json"
+with open(compiled_contract_path) as file:
+    contract_json = json.load(file)
+    contract_abi = contract_json['abi']
+contract = w3.eth.contract(address = deployed_contract_address, abi = contract_abi)
+
+
+
+'''
+#Calling a contract function createAcc(uint,uint,uint)
+txn_receipt = contract.functions.createAcc(1, 2, 5).transact({'txType':"0x3", 'from':w3.eth.accounts[0], 'gas':2409638})
+txn_receipt_json = json.loads(w3.to_json(txn_receipt))
+print(txn_receipt_json) # print transaction hash
+
+# print block info that has the transaction)
+print(w3.eth.get_transaction(txn_receipt_json)) 
+
+#Call a read only contract function by replacing transact() with call()
+
+'''
+
+#Add your Code here
