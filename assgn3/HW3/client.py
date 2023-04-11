97a4527282e6cbb9f1ffa758c140b0d319e517a8
Virendra Kabra <virendrakabra14@gmail.com>
2023-04-11 13:53:27 +0530

    modified client

--- assgn3/HW3/client.py
+++ assgn3/HW3/client.py
@@ -5,89 +5,107 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
-"""
-Functions interacting with the contract
-"""
 
-def registerUser(user_id, user_name, contract, from_acc):
-    contract.functions.registerUser(user_id, user_name).transact({'from':from_acc})
+class Interact:
+    """
+    Class for interaction with the contract
+    """
 
-def createAcc(user_id_1, user_id_2, contract, from_acc, total_balance=None, mean_balance=10):
-    if total_balance is None:
-        total_balance = int(np.random.exponential(scale=mean_balance))
-        if total_balance%2 == 1:        # extra (for equal division)
-            total_balance += 1
-    assert total_balance%2 == 0
-    contract.functions.createAcc(user_id_1, user_id_2, total_balance).transact({'from':from_acc})
+    def __init__(self, contract, from_acc):
+        self.contract = contract
+        self.from_acc = from_acc
+        self.adj_matrix = None
 
-def sendAmount(user_id_1, user_id_2, contract, from_acc, amount=1, print_error=False):
-    edges = contract.functions.getEdges().call()
-    num_nodes = len(edges)
-    index_1 = contract.functions.getIndexFromId(user_id_1).call()
-    index_2 = contract.functions.getIndexFromId(user_id_2).call()
+    def registerUser(self, user_id, user_name):
+        self.contract.functions.registerUser(user_id, user_name).transact({'from':self.from_acc})
 
-    if index_1>=num_nodes or index_2>=num_nodes:
-        if print_error:
-            print("transaction failure: nodes not present")
-        return False
-    
-    visited = [-1 for i in range(num_nodes)]    # works as both `visited` and `distance` arrays
-    parents = [set() for i in range(num_nodes)]
-    queue = []
+    def createAcc(self, user_id_1, user_id_2, total_balance=None, mean_balance=10):
+        if total_balance is None:
+            total_balance = int(np.random.exponential(scale=mean_balance))
+            if total_balance%2 == 1:        # extra (for equal division)
+                total_balance += 1
+        assert total_balance%2 == 0
+        self.contract.functions.createAcc(user_id_1, user_id_2, total_balance).transact({'from':self.from_acc})
 
-    queue.append(index_1)
-    visited[index_1] = 0
+    def get_matrix(self):
+        self.adj_matrix = self.contract.functions.getEdges().call()
 
-    # https://stackoverflow.com/questions/20257227/
-    while not len(queue) == 0:
-        node = queue.pop(0)
-        for i in range(num_nodes):
-            if edges[node][i] >= 0:             # for considering paths having
-                                                # sufficient funds, use `>= amount`
-                if visited[i] == -1:
-                    visited[i] = visited[node]+1
-                    parents[i].add(node)
-                    queue.append(i)
-                else:
-                    if visited[i] == visited[node]-1:
-                        parents[node].add(i)    # capture ALL shortest paths
-        if node == index_2:
-            break
+    def sendAmount(self, user_id_1, user_id_2, amount=1, print_error=False):
+        # Not calling getEdges() every time, as the simulation
+        # does not involve creating/closing accounts after txns start.
+        # In the general case, need to get adj_matrix each time.
 
-    # get a shortest path
-    path = []
-    node = index_2
-    while node != index_1:
-        path.insert(0, node)
-        if len(parents[node]) == 0:
+        assert self.adj_matrix is not None, "adj_matrix is None"
+
+        num_nodes = len(self.adj_matrix)
+        index_1 = self.contract.functions.getIndexFromId(user_id_1).call()
+        index_2 = self.contract.functions.getIndexFromId(user_id_2).call()
+
+        if index_1>=num_nodes or index_2>=num_nodes:
             if print_error:
-                print("transaction failure: no path found")
+                print("transaction failure: nodes not present")
             return False
-        parents[node] = sorted(parents[node], key=lambda x: -edges[x][node])
-        node = list(parents[node])[0]   # heuristic: parent with highest fund
-    path.insert(0, index_1)
+        
+        visited = [-1 for i in range(num_nodes)]    # works as both `visited` and `distance` arrays
+        parents = [set() for i in range(num_nodes)]
+        queue = []
 
-    # check for sufficient funds along this path
-    for i in range(len(path)-1):
-        if edges[path[i]][path[i+1]] < amount:
-            if print_error:
-                print("transaction failure: insufficient funds along shortest path")
-            return
+        queue.append(index_1)
+        visited[index_1] = 0
 
-    for i in range(len(path)-1):
-        id1 = contract.functions.getIdFromIndex(path[i]).call()
-        id2 = contract.functions.getIdFromIndex(path[i+1]).call()
-        try:
-            contract.functions.sendAmount(id1, id2, amount).transact({'from':from_acc})
-        except:
-            if print_error:
-                print("transaction failure: contract function raised an exception")
-            return False
-    
-    return True
+        # https://stackoverflow.com/questions/20257227/
+        while not len(queue) == 0:
+            node = queue.pop(0)
+            for i in range(num_nodes):
+                if self.adj_matrix[node][i] >= 0:   # for considering paths having
+                                                    # sufficient funds, use `>= amount`
+                    if visited[i] == -1:
+                        visited[i] = visited[node]+1
+                        parents[i].add(node)
+                        queue.append(i)
+                    else:
+                        if visited[i] == visited[node]-1:
+                            parents[node].add(i)    # capture ALL shortest paths
+            if node == index_2:
+                break
 
-def closeAccount(user_id_1, user_id_2, contract, from_acc):
-    contract.functions.closeAccount(user_id_1, user_id_2).transact({'from':from_acc})
+        # get a shortest path
+        path = []
+        node = index_2
+        while node != index_1:
+            path.insert(0, node)
+            if len(parents[node]) == 0:
+                if print_error:
+                    print("transaction failure: no path found")
+                return False
+            parents[node] = sorted(parents[node], key=lambda x: -self.adj_matrix[x][node])
+            node = list(parents[node])[0]   # heuristic: parent with highest fund
+        path.insert(0, index_1)
+
+        # check for sufficient funds along this path
+        for i in range(len(path)-1):
+            if self.adj_matrix[path[i]][path[i+1]] < amount:
+                if print_error:
+                    print("transaction failure: insufficient funds along shortest path")
+                return
+
+        for i in range(len(path)-1):
+            id1 = self.contract.functions.getIdFromIndex(path[i]).call()
+            id2 = self.contract.functions.getIdFromIndex(path[i+1]).call()
+            try:
+                self.contract.functions.sendAmount(id1, id2, amount).transact({'from':self.from_acc})
+                # if succeeds, modify the adj_matrix maintained in client
+                self.adj_matrix[id1][id2] -= amount
+                self.adj_matrix[id2][id1] += amount
+            except:
+                if print_error:
+                    print("transaction failure: contract function raised an exception")
+                return False
+        
+        return True
+
+    def closeAccount(self, user_id_1, user_id_2, from_acc):
+        self.contract.functions.closeAccount(user_id_1, user_id_2).transact({'from':from_acc})
 
 
 if __name__=="__main__":
@@ -103,7 +121,7 @@ if __name__=="__main__":
     assert w3.is_connected() == True
 
     # contract address
-    deployed_contract_address = '0x11f8e63aD45d88097d55e24047A1c092866B667A'
+    deployed_contract_address = '0x8218D770803932e684312F56aae68ee0b096aE75'
 
     # path to contract json file
     compiled_contract_path ="build/contracts/Payment.json"
@@ -123,21 +141,29 @@ if __name__=="__main__":
     ratios = {}
     ratios_moving = {}
 
+    interact_obj = Interact(contract=contract, from_acc=w3.eth.accounts[0])
+
     # register users
 
     for i in range(num_users):
-        registerUser(i, f"user{i}", contract, w3.eth.accounts[0])
+        interact_obj.registerUser(i, f"user{i}")
     user_ids = list(range(num_users))
 
     # create connected graph, and joint accounts
+
+    # NOTE: we use networkx module to create a graph
+    # according to power-law degree distribution
 
     graph = nx.powerlaw_cluster_graph(n=100, m=5, p=0.3)
     while not nx.is_connected(graph):
         graph = nx.powerlaw_cluster_graph(n=100, m=5, p=0.3)
     
     for edge in graph.edges:
-        createAcc(edge[0], edge[1], contract, w3.eth.accounts[0])   # mean balance: 10
+        interact_obj.createAcc(edge[0], edge[1])   # mean balance: 10
     
+    # get matrix
+    interact_obj.get_matrix()
+
     # perform transactions
 
     successful_in_this_interval = 0
@@ -145,7 +171,7 @@ if __name__=="__main__":
 
     for i in range(num_txns):
         user_1, user_2 = random.sample(user_ids, k=2)
-        if sendAmount(user_1, user_2, contract, w3.eth.accounts[0], amount=1):
+        if interact_obj.sendAmount(user_1, user_2, amount=1):
             successful_in_this_interval += 1
             successful_total += 1
         if i % ratio_interval == (ratio_interval-1):
